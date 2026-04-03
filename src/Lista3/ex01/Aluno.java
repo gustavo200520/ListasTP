db1f3c46300d6559e9a84ba91359bdebc122a97e
Gustavo Matos Barbosa <gustavomatosbarbosa2@gmail.com>
2026-04-03 17:12:45 -0300

    adicionando Ex01 Lista3

--- src/Lista3/ex01/Aluno.java
+++ src/Lista3/ex01/Aluno.java
@@ -1,5 +1,22 @@
 package Lista3.ex01;
 
 public class Aluno {
-
+	private String nome;
+	private double nota;
+	
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Double getNota() {
+		return nota;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
 }
--- src/Lista3/ex01/Main.java
+++ src/Lista3/ex01/Main.java
@@ -1,5 +1,31 @@
 package Lista3.ex01;
-
+import java.awt.List;
+import java.util.Scanner;
+import java.util.ArrayList;
 public class Main {
-
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		ArrayList<Aluno> listaAluno = new ArrayList();
+		int quantidade, contador=0;
+		
+		System.out.println("Insira a quantidade de alunos: ");
+		quantidade = teclado.nextInt();
+		
+		while(contador<quantidade) {
+			Aluno novoAluno = new Aluno();
+			teclado.nextLine();
+			System.out.println("Insira o nome: ");
+			novoAluno.setNome(teclado.nextLine());
+			System.out.println("Insira a nota: ");
+			novoAluno.setNota(teclado.nextDouble());
+			listaAluno.add(novoAluno);
+			contador++;
+		}
+		
+		for(Aluno alunoNovo : listaAluno) {
+			System.out.printf("Nome: %s\n",alunoNovo.getNome());
+			System.out.printf("Nota: %.2f\n",alunoNovo.getNota());
+			System.out.println("-----------------------");
+		}
+	}
 }
