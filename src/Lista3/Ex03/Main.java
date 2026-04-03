77743d556c09bef5a60b3744563bf1b3efe85b2a
Gustavo Matos Barbosa <gustavomatosbarbosa2@gmail.com>
2026-04-03 18:06:05 -0300

    adicionando Ex03 lista 3

---
+++ src/Lista3/Ex03/Main.java
@@ -0,0 +1,37 @@
+package Lista3.Ex03;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		ArrayList<Contato> listaContato = new ArrayList<>();
+		int contador=0;
+		
+		while(contador<5) {
+			Contato novoProduto = new Contato();
+			System.out.println("Insira o nome: ");
+			novoProduto.setNome(teclado.nextLine());
+			System.out.println("Insira o numero de telefone: ");
+			novoProduto.setTelefone(teclado.nextLine());
+			listaContato.add(novoProduto);
+			contador++;
+		}
+		
+		System.out.println("Verifique um nome: ");
+		String nomeAleatorio = teclado.nextLine();
+		boolean verificar=false;
+		for(Contato contatoNovo : listaContato) {
+			if(nomeAleatorio.equals(contatoNovo.getNome())) {
+				System.out.printf("Esta na lista\n");
+				System.out.printf("Nome: %s\n",contatoNovo.getNome());
+				System.out.printf("Numero: %s\n",contatoNovo.getTelefone());
+				verificar=true;
+				break;
+			}
+		}
+		if(verificar==false) {
+			System.out.println("Nao esta na lista");
+		}
+		
+	}
+}
